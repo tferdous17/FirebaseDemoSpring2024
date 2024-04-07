8439ec084981bb4894ee84182b176fc03583e014
Tasnim Ferdous <tferdous611@gmail.com>
2024-04-07 17:19:34 -0400

    refactor: tweak signIn() + registerUser() + addUserToDb(), implement retrievePersonByUuidAndReturnPass()

--- src/main/java/aydin/firebasedemospring2024/WelcomeController.java
+++ src/main/java/aydin/firebasedemospring2024/WelcomeController.java
@@ -2,10 +2,12 @@ package aydin.firebasedemospring2024;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
@@ -22,6 +24,9 @@ public class WelcomeController {
     public TextField registerPassTxt;
     public TextField registerPhoneTxt;
     public Button registerButton;
+    public TextField signInEmailTxt;
+    public TextField signInPassTxt;
+    public Button signInBtn;
 
 
     @FXML
@@ -32,7 +37,6 @@ public class WelcomeController {
     @FXML
     private void register() throws IOException {
         if (registerUser()) {
-            addUserToDb();
             registerNameTxt.clear();
             registerEmailTxt.clear();
             registerPhoneTxt.clear();
@@ -42,26 +46,22 @@ public class WelcomeController {
 
     @FXML
     private void signIn() {
-        String email = registerEmailTxt.getText();
-        String phoneNum = registerPhoneTxt.getText();
+        String email = signInEmailTxt.getText();
+        String password = signInPassTxt.getText();
 
         try {
-            UserRecord user = getUserByEmailOrPhoneNumber(email, phoneNum);
+            UserRecord user = DemoApp.fauth.getUserByEmail(email);
             if (user != null) {
-                System.out.println("Successfully signed in.");
-                switchToPrimary();
+                String uuid = user.getUid();
+                if (password.equals(retrievePersonByUuidAndReturnPass(uuid))) {
+                    switchToPrimary();
+                } else {
+                    System.out.println("ERROR: Invalid password.");
+                }
             }
         } catch (FirebaseAuthException | IOException e) {
-            System.out.println("ERROR: Could not sign in. Please check email or phone number.");
+            System.out.println("ERROR: Could not sign in.");
         }
-    }
-
-    private UserRecord getUserByEmailOrPhoneNumber(String email, String phoneNum) throws FirebaseAuthException, IOException {
-        UserRecord user = DemoApp.fauth.getUserByEmail(email);
-        if (user == null) {
-            user = DemoApp.fauth.getUserByPhoneNumber(phoneNum);
-        }
-        return user;
     }
 
     public boolean registerUser() {
@@ -76,13 +76,13 @@ public class WelcomeController {
         UserRecord userRecord;
         try {
             userRecord = DemoApp.fauth.createUser(request);
+            addUserToDb(userRecord.getUid());
             System.out.println("Successfully created new user with Firebase Uid: " + userRecord.getUid()
                     + " check Firebase > Authentication > Users tab");
             return true;
 
         } catch (FirebaseAuthException ex) {
-            // Logger.getLogger(FirestoreContext.class.getName()).log(Level.SEVERE, null, ex);
-//            System.out.println("Error creating a new user in the firebase");
+            System.out.println("Error creating a new user in the firebase");
             ex.printStackTrace();
             return false;
 
@@ -90,14 +90,41 @@ public class WelcomeController {
 
     }
 
-    public void addUserToDb() {
+    public void addUserToDb(String userUUID) {
         DocumentReference docRef = DemoApp.fstore.collection("Persons").document(UUID.randomUUID().toString());
 
         Map<String, Object> data = new HashMap<>();
+        data.put("UUID", userUUID);
         data.put("Name", registerNameTxt.getText());
         data.put("Password", registerPassTxt.getText());
 
         //asynchronously write data
         ApiFuture<WriteResult> result = docRef.set(data);
+    }
+
+    private String retrievePersonByUuidAndReturnPass(String uuid) {
+        //asynchronously retrieve all documents
+        ApiFuture<QuerySnapshot> future =  DemoApp.fstore.collection("Persons").get();
+        // future.get() blocks on response
+        List<QueryDocumentSnapshot> documents;
+        try
+        {
+            documents = future.get().getDocuments();
+            if(!documents.isEmpty()) {
+                System.out.println("Fetching data from firebase database..");
+                for (QueryDocumentSnapshot document : documents) {
+                    if (document.getData().get("UUID").equals(uuid)) {
+                        System.out.println("SUCCESS: Found person with corresponding UUID");
+                        return document.getData().get("Password").toString();
+                    }
+                }
+            } else {
+                System.out.println("No data");
+            }
+        }
+        catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 }
